e2c0cd47c8f1992c3de1d272bc87b3fc6b4a9bea
Onur-Goekcek <onurgoekcek@web.de>
2025-07-13 15:12:51 +0200

    Anfang

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool aufgabe = true;
+            string aufgabentext = aufgabenText();
+            if (aufgabe == true)
+            {
+                Console.WriteLine(aufgabentext);
+            }
+        }
+        static string aufgabenText()
+        {
+            string newAufgabe = Console.ReadLine();
+            return newAufgabe;
+        }
+    }
+}
